85a6cd48c100536ed9952df306afac8c45b4a651
Sophotheo <2559629448@qq.com>
2025-05-28 11:10:33 +0800

    feat: 5.1 函数语法

---
+++ 5. 函数/5.1 函数语法/5.1.7 文档注释.js
@@ -0,0 +1,25 @@
+/**
+ * nums: 一个数字的数组
+ */
+var nums = [3, 2, 4, 6, 1];
+
+/**
+ * 两数之和
+ * @param {number} a 第一个数
+ * @param {number} b 第二个数
+ * @returns {number} 相加的结果
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+/**
+ * 判断一个数是不是素数
+ * @param {number} n 要判断的数
+ * @returns {boolean} 是否是素数
+ */
+function isPrime(n) {
+  //..
+}
+
+var s = sum(1, 3);
