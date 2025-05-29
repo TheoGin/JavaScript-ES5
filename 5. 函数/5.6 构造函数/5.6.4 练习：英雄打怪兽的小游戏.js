b1006794b5967b5e6d7f5446ffa33dd5f108d935
Sophotheo <2559629448@qq.com>
2025-05-29 16:28:59 +0800

    fix：更改类名和变量名

--- 5. 函数/5.6 构造函数/5.6.4 练习：英雄打怪兽的小游戏.js
+++ 5. 函数/5.6 构造函数/5.6.4 练习：英雄打怪兽的小游戏.js
@@ -5,8 +5,16 @@
   创建一个英雄和一个怪兽，让它们互相攻击，直到一方死亡，输出整个攻击过程。
 */
 
-function Figure(name, attack, defense, hitPoint, criticalRate) {
-  if (new.target === Figure) {
+/**
+ * 
+ * @param {*} name 角色名
+ * @param {*} attack 攻击力
+ * @param {*} defense 防御力
+ * @param {*} hitPoint 生命值
+ * @param {*} criticalRate 暴击几率（0 ~ 100）
+ */
+function Character(name, attack, defense, hitPoint, criticalRate) {
+  if (new.target === Character) {
     this.name = name;
     this.attack = attack;
     this.defense = defense;
@@ -17,26 +25,26 @@ function Figure(name, attack, defense, hitPoint, criticalRate) {
         `${this.name}\t生命：${this.hitPoint}\t攻击：${this.attack}\t防御：${this.defense}\t暴击率：${this.criticalRate}%`
       );
     };
-    this.toAttack = function (figure) {
+    this.toAttack = function (character) {
       // 攻击伤害 = 攻击力 - 防御力
-      var damage = this.attack - figure.defense;
-      figure.hitPoint -= damage;
-      if (figure.hitPoint < 0) {
-        figure.hitPoint = 0;
+      var damage = this.attack - character.defense;
+      character.hitPoint -= damage;
+      if (character.hitPoint < 0) {
+        character.hitPoint = 0;
       }
       console.log(
-        `【${this.name}】攻击【${figure.name}】，造成【${damage}】点伤害，对方当前血量为【${figure.hitPoint}】`
+        `【${this.name}】攻击【${character.name}】，造成【${damage}】点伤害，对方当前血量为【${character.hitPoint}】`
       );
     };
-    this.toCritical = function (figure) {
+    this.toCritical = function (character) {
       // 暴击几率（暴击时伤害翻倍）
-      var damage = (this.attack - figure.defense) * 2;
-      figure.hitPoint -= damage;
-      if (figure.hitPoint < 0) {
-        figure.hitPoint = 0;
+      var damage = (this.attack - character.defense) * 2;
+      character.hitPoint -= damage;
+      if (character.hitPoint < 0) {
+        character.hitPoint = 0;
       }
       console.log(
-        `暴击！【${this.name}】攻击【${figure.name}】，造成【${damage}】点伤害，对方当前血量为【${figure.hitPoint}】`
+        `暴击！【${this.name}】攻击【${character.name}】，造成【${damage}】点伤害，对方当前血量为【${character.hitPoint}】`
       );
     };
   } else {
@@ -55,15 +63,27 @@ function Figure(name, attack, defense, hitPoint, criticalRate) {
   }
 }
 
-var cheng = new Figure("成哥", 100, 20, 500, 30);
+var cheng = new Character("成哥", 100, 20, 500, 30);
 cheng.introduce();
 
 console.log("VS");
 
-var deng = new Figure("邓哥", 120, 40, 400, 5);
+var deng = new Character("邓哥", 120, 40, 400, 5);
 deng.introduce();
 
-cheng.toCritical(deng);
+while (cheng.hitPoint > 0 && deng.hitPoint > 0) {
+  var randomNum = Math.random();
+  if (randomNum < 0.2222) {
+    cheng.toAttack(deng);
+  } else if (randomNum < 0.5555) {
+    deng.toAttack(cheng);
+  } else if (randomNum < 0.7777) {
+    cheng.toCritical(deng);
+  } else {
+    deng.toCritical(cheng);
+  }
+}
+/* cheng.toCritical(deng);
 
 deng.toAttack(cheng);
 
@@ -75,9 +95,9 @@ cheng.toCritical(deng);
 
 deng.toAttack(cheng);
 
-cheng.toCritical(deng);
+cheng.toCritical(deng); */
 
 console.log("========================");
 cheng.introduce();
 deng.introduce();
-console.log('游戏结束！');
+console.log("游戏结束！");
