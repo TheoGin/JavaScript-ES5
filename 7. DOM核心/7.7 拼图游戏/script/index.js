70e09748a6618018639deb0a16d5d797c19d1aac
Sophotheo <2559629448@qq.com>
2025-06-08 15:31:30 +0800

    feat: 7.7 拼图游戏

--- 7. DOM核心/7.7 拼图游戏/script/index.js
+++ 7. DOM核心/7.7 拼图游戏/script/index.js
@@ -1,149 +1,131 @@
-/**
- * 全局配置
- */
-var config = {
-    width: 500,
-    height: 500,
-    row: 3, //行数
-    col: 3, //列数
-    img: "./img/lol.png", //图片路径
-    gameDom: document.getElementById("game") //游戏的根元素
-}
-// 每一个小块的宽度
-config.blockWidth = config.width / config.col;
-// 每一个小块的高度
-config.blockHeight = config.height / config.row;
+var divGame = document.getElementById("game");
+divGame.style.width = "500px";
+divGame.style.height = "500px";
+divGame.style.border = "2px solid #ccc";
+divGame.style.position = "relative";
 
-config.total = config.row * config.col; //总数
+var h1 = document.querySelector("h1");
 
-var emptyBlock; //空白元素
+// var img = document.createElement('img')
+// img.src = 'img/lol.png'
+// divGame.appendChild(img)
 
-/**
- * 生成一个小方块的函数，直接添加到gameDom中
- */
-function createBlockDom(x, y, apendToPage, correctX, correctY) {
-    var dom = document.createElement("div");
-    dom.style.width = config.blockWidth + "px";
-    dom.style.height = config.blockHeight + "px";
-    dom.style.background = `url(${config.img})`;
-    dom.style.border = "1px solid #fff";
-    //设置该样式，是为了让宽高包含边框的尺寸
-    dom.style.boxSizing = "border-box";
-    dom.style.position = "absolute";
-    //1. 元素位置？ 从一个有限的坐标中随机取一个
-    dom.style.left = x + "px";
-    dom.style.top = y + "px";
-    dom.correctX = correctX;
-    dom.correctY = correctY;
-    //2. 背景图位置？根据正确位置来算
-    dom.style.backgroundPosition = `-${correctX}px -${correctY}px`;
-    dom.style.cursor = "pointer";
+// 500 * 500 九等分
 
-    dom.onclick = function () {
-        // 将当前元素的坐标，与空白元素的坐标交换
-        //判断是否相邻
-        var xdis = Math.abs(parseFloat(dom.style.left) - parseFloat(emptyBlock.style.left))
-        xdis = parseInt(xdis);
-        var ydis = Math.abs(parseFloat(dom.style.top) - parseFloat(emptyBlock.style.top))
-        ydis = parseInt(ydis);
-
-        if (xdis + ydis !== parseInt(config.blockHeight) &&
-            xdis + ydis !== parseInt(config.blockWidth)) {
-            return;
-        }
-
-        var x = dom.style.left;
-        var y = dom.style.top;
-        dom.style.left = emptyBlock.style.left;
-        dom.style.top = emptyBlock.style.top;
-        emptyBlock.style.left = x;
-        emptyBlock.style.top = y;
-        if (isWin()) {
-            document.querySelector("h1").innerHTML = "德玛西亚！！！";
-        }
-    }
-
-    if (apendToPage) {
-        config.gameDom.appendChild(dom);
-    }
-    else {
-        emptyBlock = dom;
-    }
+function createDiv(position) {
+  //   console.log(position);
+  var div = document.createElement("div");
+  div.style.width = "165px";
+  div.style.height = "165px";
+  var src = "url(img/lol.png)";
+  div.style.backgroundImage = src;
+  div.style.backgroundPosition = `${position.x}px ${position.y}px `;
+  div.style.border = "1px solid #fff";
+  div.style.cursor = "pointer";
+  div.style.boxSizing = "border-box";
+  // div.style.float = "left";
+  div.style.position = "absolute";
+  return div;
 }
 
-function isWin() {
-    for (let i = 0; i < config.gameDom.children.length; i++) {
-        const dom = config.gameDom.children[i];
-        if (parseInt(dom.correctX) !== parseInt(dom.style.left) ||
-            parseInt(dom.correctY) !== parseInt(dom.style.top)) {
-            return false;
-        }
+var imgs = [];
+var positions = [];
+var size = parseInt(500 / 3);
+for (var i = 0; i < size * 3; i += size) {
+  for (var j = 0; j < size * 3; j += size) {
+    // console.log(i, j);
+    if (i === size && j === size) {
+      //   console.log(i, j); // 166.66666666666666 166.66666666666666
+      continue;
     }
+    imgs.push(createDiv({ x: i, y: j }));
+  }
+}
+for (var i = 0; i < size * 3; i += size) {
+  for (var j = 0; j < size * 3; j += size) {
+    if (i === size * 2 && j === size * 2) {
+      continue;
+    }
+    console.log(i, j);
+    positions.push({ x: i, y: j });
+  }
+}
+
+// 打乱顺序
+positions.sort(function () {
+  return Math.random() - 0.5;
+});
+
+// 空白位置
+var space = {
+  // 333.333
+  x: size * 2,
+  y: size * 2,
+};
+
+var i = 0;
+positions.forEach(function (position) {
+  // console.log(imgs[i], position.x);
+  imgs[i].style.top = position.y + "px";
+  imgs[i].style.left = position.x + "px";
+  i++;
+});
+
+var frag = document.createDocumentFragment();
+imgs.forEach(function (img) {
+  img.onclick = function () {
+    var top = parseInt(this.style.top);
+    var left = parseInt(this.style.left);
+    // console.log(top);
+    // console.log(left);
+    /* if (
+      (top === space.x && left + size === space.y) ||
+      (top + size === space.y && left === space.x) ||
+      (top === space.x && left - size === space.y) ||
+      (top - size === space.y && left === space.x)
+    ) { */
+    if (
+      top + left + size === space.x + space.y ||
+      top + left - size === space.x + space.y
+    ) {
+      var tmp = { x: left, y: top };
+      this.style.left = space.x + "px";
+      this.style.top = space.y + "px";
+      space = tmp;
+    }
+    if (isFinish()) {
+      h1.innerHTML = "德玛西亚！！！";
+    }
+  };
+  frag.appendChild(img);
+});
+divGame.appendChild(frag);
+
+function isFinish() {
+  var cnt = 0;
+  imgs.forEach(function (img) {
+    var top = parseInt(img.style.top);
+    var left = parseInt(img.style.left);
+    var x = parseInt(img.style.backgroundPositionX);
+    var y = parseInt(img.style.backgroundPositionY);
+    console.log(top);
+    console.log(left);
+    console.log(x);
+    console.log(y);
+    console.log("---------------");
+    if (left === x && top === y) {
+      cnt++;
+    }
+  });
+  console.log(cnt);
+
+  if (cnt === 8) {
     return true;
+  } else {
+    return false;
+  }
 }
 
-/**
- * 得到一个数组，数组中包含所有正确的坐标
- */
-function getCorrectPoints() {
-    var arr = [];
-    //循环行和列
-    for (var i = 0; i < config.row; i++) {
-        for (var j = 0; j < config.col; j++) {
-            arr.push({
-                x: j * config.blockWidth,
-                y: i * config.blockHeight
-            })
-        }
-    }
-    return arr;
+if (isFinish()) {
+  h1.innerHTML = "德玛西亚！！！";
 }
-
-/**
- * 根据最小值和最大值（取不到）得到一个随机数
- * @param {*} min 
- * @param {*} max 
- */
-function getRandom(min, max) {
-    var dec = max - min;
-    return Math.floor(Math.random() * dec + min);
-}
-
-/**
- * 洗牌
- */
-function shuffle(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        const ele = arr[i];
-        //随机生成下标，交换
-        var index = getRandom(0, arr.length - 1);
-        arr[i] = arr[index];
-        arr[index] = ele;
-    }
-}
-
-/**
- * 生成游戏区域
- */
-function setGameArea() {
-    //1. 初始化游戏根元素
-    config.gameDom.style.width = config.width + "px";
-    config.gameDom.style.height = config.height + "px";
-    config.gameDom.style.border = "2px solid #ccc";
-    config.gameDom.style.position = "relative";
-    //2. 生成所有小方块（gameDom的子元素）
-    var points = getCorrectPoints(); //得到正确坐标的数组
-    shuffle(points);
-    var correctPoints = getCorrectPoints();
-    for (let i = 0; i < config.total; i++) {
-        if (i < config.total - 1) {
-            //不是最后生成的方块
-            createBlockDom(points[i].x, points[i].y, true, correctPoints[i].x, correctPoints[i].y);
-        }
-        else {
-            createBlockDom(points[i].x, points[i].y, false, correctPoints[i].x, correctPoints[i].y);
-        }
-    }
-}
-
-setGameArea();
