8406ee5226ae351b383f4220281d8863e8ec2772
Sophotheo <2559629448@qq.com>
2025-06-09 16:52:27 +0800

    fix: 7.7 拼图游戏2

--- 7. DOM核心/7.7 拼图游戏/script/index.js
+++ 7. DOM核心/7.7 拼图游戏/script/index.js
@@ -8,7 +8,7 @@ var gameConfig = {
   cols: 3, // 列数
   // imgSrc: '../img/lol.png', // 错误写法，因为js是要看运行时候的路径，而不是书写的路径，运行的时候是放到页面运行的
   imgSrc: "img/lol.png", // 图片路径
-  dom: document.getElementById("game"), // 拿到div的dom对象
+  dom: document.getElementById("game"), // 拿到div游戏的dom对象
   // pieceWidth: width / 3, // Uncaught ReferenceError: width is not defined
   // pieceWidth: this.width / 3, // pieceWidth: NaN，因为this只能在方法中用——》通过创建完对象之后，动态添加属性的方式添加pieceWidth
   isOver: false, // 用于记录游戏是否结束，如果结束则不能再触发点击事件
@@ -18,21 +18,30 @@ var gameConfig = {
 gameConfig.pieceWidth = gameConfig.width / gameConfig.cols; // 每一个小方块的宽度
 gameConfig.pieceHeight = gameConfig.height / gameConfig.rows; // 每一个小方块的高度
 
-// 小方块数组
+// 包含小方块信息的数组
 var blocks = [];
 
 /**
- * 创建每一个小方块元素，离左边、上边、正确左边、正确上边的距离
+ * 判断两个小方块是否相等
+ * @param {*} num1
+ * @param {*} num2
+ */
+function isEqual(num1, num2) {
+  return parseInt(num1) === parseInt(num2);
+}
+
+/**
+ * 小方块的构造函数。创建每一个小方块元素，离左边、上边、正确左边、正确上边的距离
  * @param {*} left
  * @param {*} top
- * @param {*} isVisible 用于控制最后一块不显示
+ * @param {*} isVisible 是否可见。用于控制最后一块不显示
  */
 function Block(left, top, isVisible) {
-  this.left = left;
-  this.top = top;
-  this.isVisible = isVisible; // 用于查找空白位置小方块
-  this.correctLeft = this.left; // 初始正确离左边距离就是left
-  this.correctTop = this.top; // 初始正确离顶部距离就是top
+  this.left = left; // 当前的横坐标
+  this.top = top; // 当前的纵坐标
+  this.isVisible = isVisible; // 是否可见。用于查找空白位置小方块
+  this.correctLeft = this.left; // 正确的横坐标。初始正确离左边距离就是left
+  this.correctTop = this.top; // 正确的纵坐标。初始正确离顶部距离就是top
 
   this.dom = document.createElement("div");
   this.dom.style.width = gameConfig.pieceWidth + "px";
@@ -43,59 +52,29 @@ function Block(left, top, isVisible) {
   // 边框会占据尺寸
   this.dom.style.boxSizing = "border-box";
   this.dom.style.background = `url("${gameConfig.imgSrc}") -${this.correctLeft}px -${this.correctTop}px`;
+  // 过渡效果
+  this.dom.style.transition = ".5s"; //css属性变化的时候，在0.5秒中内完成
   if (!isVisible) {
     this.dom.style.display = "none";
   }
 
   // 定位的位置不要直接用属性赋值，因为后面需要随机更改位置
   this.show = function () {
+    // 根据当前的left、top，重新设置div的位置
     this.dom.style.left = this.left + "px";
     this.dom.style.top = this.top + "px";
   };
+
+  // 判断当前方块是否在正确的位置上
+  this.isCorrect = function () {
+    return (
+      isEqual(this.left, this.correctLeft) && isEqual(this.top, this.correctTop)
+    );
+  };
+
   this.show();
 
   gameConfig.dom.appendChild(this.dom);
-}
-
-/**
- * 生成min到max的随机数（包含max）
- * @param {*} min
- * @param {*} max
- */
-function getRandom(min, max) {
-  return Math.floor(min + Math.random() * (max + 1 - min));
-}
-
-/**
- * 用当前小方块1与小方块2的位置进行交换
- * @param {*} block1
- * @param {*} block2
- */
-function exchange(block1, block2) {
-  // 1) 交换左边
-  var tmp = block1.left;
-  block1.left = block2.left;
-  block2.left = tmp;
-  // 2) 交换上边
-  tmp = block1.top;
-  block1.top = block2.top;
-  block2.top = tmp;
-
-  // block1.show();
-  // 交换完需要全部重新设置位置，不然会出问题
-  blocks.forEach(function (block) {
-    // 交换完重新设置展示
-    block.show();
-  });
-}
-
-/**
- * 判断两个小方块是否相等
- * @param {*} float1
- * @param {*} float2
- */
-function isEqual(float1, float2) {
-  return parseInt(float1) === parseInt(float2);
 }
 
 /**
@@ -103,11 +82,11 @@ function isEqual(float1, float2) {
  */
 function init() {
   // 1. 初始化游戏容器
-  initGameContainer();
+  initGameDom();
 
   // 2. 初始化小方块
-  // 2.1 初始化小方块数组
-  initBlockArray();
+  // 2.1 初始化小方块数组。准备好一个数组，数组的每一项是一个对象，记录了每一个小方块的信息
+  initBlocksArray();
 
   // 2.2 打乱小方块顺序
   shuffle();
@@ -119,7 +98,7 @@ function init() {
    * 注册点击事件
    */
   function regEvent() {
-    // 只找一个，用find方法
+    // 找到看不见的方块。只找一个，用find方法
     var notVisibleBlock = blocks.find(function (block) {
       // return block.dom.style.display === 'none';
       // 或者
@@ -141,7 +120,7 @@ function init() {
           left: 2w, top: 0  （非小方块）
       ——> 相邻的有一个方向相等，另一个方向差的绝对值等于w/h
         */
-        // 判断相等需要注意小数问题，需要用parseInt转为整数比较，又因为后面判断是否结束游戏也需要用这个相等比较——》抽离成函数
+        // 判断是可以交换。判断相等需要注意小数问题，需要用parseInt转为整数比较，又因为后面判断是否结束游戏也需要用这个相等比较——》抽离成函数
         //  if(同一行 || 同一列)
         if (
           (block.left === notVisibleBlock.left &&
@@ -155,13 +134,79 @@ function init() {
               gameConfig.pieceWidth
             ))
         ) {
+          // 交换当前方块和看不见的方块的坐标位置
           exchange(block, notVisibleBlock);
 
-          // 是否赢
+          // 游戏结束判定。是否赢
           isWin();
         }
       };
     });
+  }
+
+  /**
+   * 游戏结束判定。是否拼图完成
+   */
+  function isWin() {
+    /* var correctBlocks= blocks.filter(function (block) {
+      return block.isCorrect();
+    }); 
+    
+    // console.log(correctBlocks.length);
+    // if (correctBlocks.length === blocks.length) {
+    */
+    var wrongsBlocks = blocks.filter(function (block) {
+      return !block.isCorrect();
+    });
+    // 或者写错误的，就判断等于0即可
+    if (wrongsBlocks.length === 0) {
+      // 用于记录游戏是否结束，如果结束则不能再触发点击事件
+      gameConfig.isOver = true;
+
+      // 游戏结束，去掉所有边框
+      blocks.forEach(function (block) {
+        block.dom.style.border = "none";
+
+        // 把空白块的元素也显示出来
+        block.dom.style.display = "block";
+      });
+    }
+  }
+
+  /**
+   * 生成min到max的随机数（包含max）
+   * @param {*} min
+   * @param {*} max
+   */
+  function getRandom(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+  }
+
+  /**
+   * 用当前小方块1与小方块2的位置进行交换
+   * @param {*} block1
+   * @param {*} block2
+   */
+  function exchange(block1, block2) {
+    // 1) 交换左边
+    var tmp = block1.left;
+    block1.left = block2.left;
+    block2.left = tmp;
+    // 2) 交换上边
+    tmp = block1.top;
+    block1.top = block2.top;
+    block2.top = tmp;
+
+    /* // block1.show();
+    // 交换完需要全部重新设置位置，不然会出问题
+    blocks.forEach(function (block) {
+      // 交换完重新设置展示
+      block.show();
+    }); */
+
+    // 两个交换完都要重新设置位置，不然会出问题
+    block1.show();
+    block2.show();
   }
 
   /**
@@ -174,7 +219,7 @@ function init() {
       // var index = getRandom(0, blocks.length - 1);
       var index = getRandom(0, blocks.length - 2); // blocks.length - 2是让最后一项不打乱位置
 
-      // 用当前小方块与随机小方块的位置进行交换
+      // 用当前小方块与随机小方块的位置交换left和top值
       exchange(blocks[i], blocks[index]);
       /* // 1) 交换左边
       var tmp = blocks[i].left;
@@ -205,7 +250,7 @@ function init() {
     ...
    ]
    */
-  function initBlockArray() {
+  function initBlocksArray() {
     for (var i = 0; i < gameConfig.rows; i++) {
       for (var j = 0; j < gameConfig.cols; j++) {
         /* w: 一个小方块的宽度；h：一个小方块的高度
@@ -224,6 +269,7 @@ function init() {
           correctTop: 0, // 初始正确离顶部距离就是top ————> 为了方便，写成一个对象
         }); */
         var isVisible = true; // 用于控制最后一块不显示
+        // i 行号，j 列号
         if (i === gameConfig.rows - 1 && j === gameConfig.cols - 1) {
           isVisible = false;
         }
@@ -240,7 +286,7 @@ function init() {
   /**
    * 初始化游戏容器
    */
-  function initGameContainer() {
+  function initGameDom() {
     gameConfig.dom.style.width = gameConfig.width + "px";
     gameConfig.dom.style.height = gameConfig.height + "px";
     gameConfig.dom.style.border = "2px solid #ccc";
@@ -249,24 +295,3 @@ function init() {
 }
 
 init();
-
-/**
- * 是否拼图完成
- */
-function isWin() {
-  var correctBlocks = blocks.filter(function (block) {
-    return block.left === block.correctLeft && block.top === block.correctTop;
-  });
-  // console.log(correctBlocks.length);
-  if (correctBlocks.length === blocks.length) {
-    // 用于记录游戏是否结束，如果结束则不能再触发点击事件
-    gameConfig.isOver = true;
-
-    blocks.forEach(function (block) {
-      block.dom.style.border = "none";
-
-      // 把空白块的元素也显示出来
-      block.dom.style.display = "block";
-    });
-  }
-}
